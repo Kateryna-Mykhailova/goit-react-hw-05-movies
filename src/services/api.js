bb21d45f8ce9b91df34d55d1a806611e89997ad9
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2022-01-24 13:20:00 +0100

    ltkftn fetchCast и рендерит Cast page

--- src/services/api.js
+++ src/services/api.js
@@ -1,52 +1,51 @@
 function fetchMovies() {
-  // console.log(page);
   return fetch(
     `https://api.themoviedb.org/3/trending/all/day?api_key=30d28e0664c735075082dfc414f074e3`,
   ).then(response => {
     if (response.ok) {
       return response.json();
-      // console.log(response.json());
     }
-    // return Promise.reject(
-    //   new Error(`There is no information for this query`),
-    // );
+    return Promise.reject(new Error(`There is no information for this query`));
   });
 }
 
 function fetchMovieDetails(movieId) {
-  // console.log(page);
   return fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=30d28e0664c735075082dfc414f074e3&language=en-US`,
   ).then(response => {
     if (response.ok) {
       return response.json();
-      // console.log(response.json());
     }
-    // return Promise.reject(
-    //   new Error(`There is no information for this query`),
-    // );
+    return Promise.reject(new Error(`There is no information for this query`));
   });
 }
 
 function fetchReviews(movieId) {
-  // console.log(page);
   return fetch(
     `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=30d28e0664c735075082dfc414f074e3&language=en-US&page=1`,
   ).then(response => {
     if (response.ok) {
       return response.json();
-      // console.log(response.json());
     }
-    // return Promise.reject(
-    //   new Error(`There is no information for this query`),
-    // );
+    return Promise.reject(new Error(`There is no information for this query`));
   });
 }
 
+function fetchCast(movieId) {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=30d28e0664c735075082dfc414f074e3&language=en-US`,
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(new Error(`There is no information for this query`));
+  });
+}
 const api = {
   fetchMovies,
   fetchMovieDetails,
   fetchReviews,
+  fetchCast,
 };
 
 export default api;
--- src/views/Cast/Cast.jsx
+++ src/views/Cast/Cast.jsx
@@ -1,17 +1,59 @@
 
 
-import React from 'react';
+import { useParams, NavLink, Link, Outlet } from 'react-router-dom';
+
+import React, { useState, useEffect } from 'react';
+import searchApi from '../../services/api'
+import { toast } from 'react-toastify';
         
 
 
-const Cast = () => {
+export default function Cast() {
    
+    const [cast, setCast] = useState(null)
+    const { movieId } = useParams();
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        searchApi
+            .fetchCast(movieId)
+            .then(({cast} )=> {
+                setCast(cast)
+            
+            })
+   
+            .catch(error => setError(error.message))
+        
+      
+    }, [movieId]);
+console.log(cast);
+
     return (
-      <main style={{ padding: "1rem" }}>
-          <p>Cast Cast Cast Cast</p>
-</main>
+        <>
+            {cast ? (
+                <ul>
+        
+                    {cast.map(cast => (
+                        <li key={cast.id} >
+                            <img style={{
+                                width: 100,
+                                height: 150
+                            }}
+                
+                                src={
+                                    cast.profile_path
+                                        ? `https://image.tmdb.org/t/p/w500/${cast.profile_path}`
+                                        : <p>No img</p>
+                                }
+                                alt={cast.name}
+                            />
+                            <h2>{cast.name}</h2>
+                            <p>character: {cast.character}</p>
+                        </li>
+                    ))}
+                </ul>) : (<p>There is no information here</p>)}
+        </>
          
        
     )
 }
-export default Cast;
